8fe15cc5873223bca0d92316b8b02220b9604aff
ivan <popov.ivan.dnr@gmail.com>
2020-02-12 22:48:09 +0300

    finish homework5

--- src/com/geekbrains/myhomework/Main.java
+++ src/com/geekbrains/myhomework/Main.java
@@ -3,6 +3,24 @@ package com.geekbrains.myhomework;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        final int size = 10000000;
+        final int h = size / 2;
+        float[] arr = new float[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+
+        FirstMethod firstMethod = new FirstMethod(size, arr);
+        Thread methodFirstThread = new Thread(firstMethod);
+        methodFirstThread.start();
+        try {
+            methodFirstThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        SecondMethod secondMethod = new SecondMethod(size, arr, h);
+        secondMethod.runSecondMethod();
     }
 }
